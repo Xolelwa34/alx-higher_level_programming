db75fe49f58226964f23cfd447d7add3bb5384dd
Xolelwa34 <ciliaswekile@gmail.com>
2023-11-28 07:27:48 -0800

    To uppercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    return(ord("char") <= ord(c) <= ord("z"))
+    if ord(c) > 96:
+        return True
+    else:
+        return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord("char") <= ord(c) <= ord("z"):
+            c = chr(ord(c) + (ord("CHAR") - ord("char")))
+        print("{:s}".format(c), end="")
+    print("")
