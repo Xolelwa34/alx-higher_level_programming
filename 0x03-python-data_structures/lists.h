7e0ee6fcff092035a6e6596ac912714e7d3b69b9
Xolelwa34 <ciliaswekile@gmail.com>
2023-12-01 06:12:42 -0800

    Print a list of integers

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+typedef struct listint_s
+{
+	int n;
+	struct lint_s *next;
+}listint_t;
+
+void free_listint(listint_t *head);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_ **head, const int n);
+int is_palindrome(listint_t **head);
+
+#endif /* LITS_H */
