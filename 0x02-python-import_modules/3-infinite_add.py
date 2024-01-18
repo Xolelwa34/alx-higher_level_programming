4acc7959b9c008d5a0841800375dcc1689c74a2c
Xolelwa34 <ciliaswekile@gmail.com>
2024-01-18 06:01:50 -0800

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_arg(argv):
+    ret = len(argv) - 1
+    if ret == 0:
+        print("{}".format(ret))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= ret:
+            add += int(argv[i])
+            i += 1
+        print("{}".format(add))
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
