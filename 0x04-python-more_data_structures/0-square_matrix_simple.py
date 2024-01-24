328f472698fded1e86dd01e670ab39e2f5530f0d
Xolelwa34 <ciliaswekile@gmail.com>
2024-01-24 08:02:33 -0800

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+new_matrix = []
+    for row in matrix:
+        sub_matrix = map(lambda num: num**2, row)
+        new_matrix.append(list(sub_matrix))
+    return new_matrix
