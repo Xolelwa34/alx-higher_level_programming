271fca59943b41fd947363eb21a13a91ff91cfce
Xolelwa34 <ciliaswekile@gmail.com>
2024-02-19 12:16:05 -0800

    Square #1

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+"""Defines a rectangle BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""Defines a class BaseGeometry."""
 
 
 class Rectangle(BaseGeometry):
