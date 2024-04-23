b7609c5846c83ce4cef61e29a24e4b6cdf75b391
Xolelwa34 <ciliaswekile@gmail.com>
2024-04-23 10:57:39 -0700

    Square#1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (l) {
+    if (l === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) 
+	    (console.log(l.repeat(this.width));
+    }
+  }
+};
