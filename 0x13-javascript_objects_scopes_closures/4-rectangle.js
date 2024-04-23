680a21dd93db4b79e2cad467be683296cebbcdea
Xolelwa34 <ciliaswekile@gmail.com>
2024-04-23 07:56:29 -0700

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-	 constructor (w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
@@ -12,5 +12,14 @@ module.exports = class Rectangle {
     for (let i = 0; i < this.height; i++) {
       console.log(l.repeat(this.width));
     }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
   }
 };
