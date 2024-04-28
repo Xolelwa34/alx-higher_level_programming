fcfbc6ef9fb15e9b1325896b0c9e39184c4fc108
Wandzfuture <michael.zabas@gmail.com>
2024-04-28 10:15:55 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""Takes argument and display all values in the states."""
-import MySQLdbfrom sys 
-import argv
+""" Takes argument and display all values in the states."""
+
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
-                (argv[4], ))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \FROM `states` \WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists all cities in the from the database htbn_0e_4_usa."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in cur.fetchall()]
