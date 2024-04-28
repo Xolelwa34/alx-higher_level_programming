6af9b0a7ba0362bc43fb0dee45a8067c332e26c0
Wandzfuture <michael.zabas@gmail.com>
2024-04-28 10:03:24 -0700

    SQL injection

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Takes argument and display all values in the states."""
-
-import MySQLdb
-from sys import argv
+import MySQLdbfrom sys 
+import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
