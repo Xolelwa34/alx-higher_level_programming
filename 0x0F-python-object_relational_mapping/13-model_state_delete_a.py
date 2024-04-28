492688311935fa4385d2d6afff7a2d05551befca
Wandzfuture <michael.zabas@gmail.com>
2024-04-28 13:10:20 -0700

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states objects form the database"""
+"""Deletes all states object with a name containing the letter a from te=he database htbn_0e_6_usa"""
 
 import sys
 from sqlalchemy import create_engine
@@ -13,5 +13,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    for state in session.query(State):
+        if "a" in state.name:
+            session.delete(state)
+    session.commit()
