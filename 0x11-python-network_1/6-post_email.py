c784a9a8dc7d1de7593dcdfbeff70173d5c367d7
Wandzfuture <michael.zabas@gmail.com>
2024-05-10 08:40:57 -0700

    POST na email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes in a URL and an email address, send POSTS tot the passed URL.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
