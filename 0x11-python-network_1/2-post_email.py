c79e871f6c459f03c90ed89c9a54c4bbde0cc348
Wandzfuture <michael.zabas@gmail.com>
2024-05-10 07:30:52 -0700

    Whats my status

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,8 @@
-#!usr/bin/python3
-"""takes in a URL and an email, sends a POST request
- And displays the body of the response
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
 import sys
 import urllib.parse
