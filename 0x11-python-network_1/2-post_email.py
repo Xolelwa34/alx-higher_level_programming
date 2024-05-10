d467e8ebf7d1d954efd52b644a2bf7347e527d8e
Wandzfuture <michael.zabas@gmail.com>
2024-05-10 07:12:15 -0700

    POST email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+"""Takes in a URL and email, sends a POST to the passed URL with an email as a parameter
+and displays the body of the response (decoded in utf-8
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
