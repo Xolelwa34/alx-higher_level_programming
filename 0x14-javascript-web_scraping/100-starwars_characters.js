2b68f478f75472bd660a2975bbd6634f9b489a34
Xolelwa34 <ciliaswekile@gmail.com>
2024-06-04 05:02:56 -0700

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,7 +7,7 @@ request(starWarsUri, function (_err, _res, body) {
   const characters = JSON.parse(body).characters;
 
   for (let c = 0; c < characters.length; ++c) {
-    request(characters[i], function (_cErr, _cRes, cBody) {
+    request(characters[c], function (_cErr, _cRes, cBody) {
       console.log(JSON.parse(cBody).name);
     });
   }
